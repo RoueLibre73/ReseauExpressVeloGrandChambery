4780325f4ea0f1d4de3cc52343a7e5522f2c177e
RoueLibre73 <45666255+RoueLibre73@users.noreply.github.com>
2025-01-29 09:49:30 +0100

    correction ] manquant

--- composables/useMap.ts
+++ composables/useMap.ts
@@ -723,7 +723,7 @@ function drawLanesVariantePostponed(map: Map, lanes: DisplayedLane[]) {
     paint: {
       'line-width': laneWidth,
       'line-color': ["to-color", ['get', 'color']],
-      'line-dasharray': [0.4, 1.1,
+      'line-dasharray': [0.4, 1.1],
       'line-offset': ['-', ['*', ['get', 'lane_index'], laneWidth], ['/', ['*', ['-', ['get', 'nb_lanes'], 1], laneWidth], 2]],
     }
   });
