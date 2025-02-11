389461c0d11e495ec4f82b92ae24c4b1b1066939
RoueLibre73 <45666255+RoueLibre73@users.noreply.github.com>
2025-02-11 22:13:50 +0100

    test suite

--- composables/useMap.ts
+++ composables/useMap.ts
@@ -884,7 +884,7 @@ function drawLanesWIP(map: Map, lanes: DisplayedLane[]) {
     paint: {
       'line-width': laneWidth,
       'line-color': ["to-color", ['get', 'color']],
-      'line-dasharray': [2, 4],
+      'line-dasharray': [1, 1],
       'line-offset': ['-', ['*', ['get', 'lane_index'], laneWidth], ['/', ['*', ['-', ['get', 'nb_lanes'], 1], laneWidth], 2]],
     }
   });
@@ -903,6 +903,22 @@ function drawLanesWIP(map: Map, lanes: DisplayedLane[]) {
   layersWithLanes.push("layer-lanes-wip-done")
 
   animateOpacity(map, 0, 750, 'layer-lanes-wip-done', 'line-opacity', 0.5, 0.9);
+  // Ajout d'une animation de mouvement pour simuler les lignes en mouvement
+map.on('style.load', () => {
+  let dashArraySequence = [
+    [0, 4, 3], [1, 4, 2], [2, 4, 1], [3, 4, 0]
+  ];
+  let step = 0;
+
+  function animateDashArray(timestamp) {
+    let newStep = (step + 1) % dashArraySequence.length;
+    map.setPaintProperty('layer-lanes-wip', 'line-dasharray', dashArraySequence[newStep]);
+    step = newStep;
+    requestAnimationFrame(animateDashArray);
+  }
+
+  animateDashArray();
+});
 }
 
 function addListnersForHovering(map: Map) {
