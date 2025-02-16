44528596299302a2a7e1947b34a7b32179eb0210
RoueLibre73 <45666255+RoueLibre73@users.noreply.github.com>
2025-02-16 22:14:01 +0100

    test image plan des pistes + lien page accueil

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -70,14 +70,14 @@
           <div
             class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center"
           >
-            <div
-              class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md"
-            >
-              <div
-                class="relative block w-full bg-white rounded-lg overflow-hidden"
-              >
-                <img class="w-full" src="https://rouelibre.net/wp-content/uploads/2024/11/hero.jpeg" :alt="`illustration des ${getRevName()}`">
-              </div>
+             <div class="relative mx-auto w-full rounded-lg shadow-lg">
+              <NuxtLink to="/carte-interactive" class="relative block w-full bg-white rounded-lg overflow-hidden">
+                <img
+                  class="w-full opacity-90"
+                  src="https://rouelibre.net/wp-content/uploads/2025/02/PlanDesPistes.png"
+                  alt="Plan des Pistes"
+                />
+              </NuxtLink>
             </div>
           </div>
         </div>
