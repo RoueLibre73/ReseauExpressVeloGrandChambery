4fff1ef957e63300930aaa3f346f4c6c258c5718
RoueLibre73 <45666255+RoueLibre73@users.noreply.github.com>
2026-02-02 21:34:28 +0100

    legende points noirs v2

--- components/LegendInfo.vue
+++ components/LegendInfo.vue
@@ -65,11 +65,12 @@
 
           <!-- Points noirs / danger -->
           <div class="my-auto flex justify-center">
-            <span icon-image="danger-icon" class="h-5 w-5"></span>
+            <div class="danger-icon h-5 w-5 bg-contain bg-no-repeat bg-center"></div>
           </div>
           <div>
             points noirs
           </div>
+          
             </div>
           </div>
 
